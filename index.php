8bf06d2aff871a0364eddb775d1fa15ec0e7d88e
ravisharmaa <bastola.ravi@gmail.com>
2018-07-08 15:55:47 +0545

    Filter

--- index.php
+++ index.php
@@ -7,11 +7,13 @@ class Product
 
     public $name;
     public $price;
+    public $outOfStock;
 
-    public function __construct($name, $price)
+    public function __construct(String $name, int $price, bool $outOfStock)
     {
         $this->name = $name;
         $this->price = $price;
+        $this->outOfStock = $outOfStock;
     }
 
     public function delete()
@@ -21,17 +23,19 @@ class Product
 }
 
 
+
 $productData = [
-    new Product('Radio', 122),
-    new Product('Walkman', 150),
-    new Product('Watch', 200),
-    new Product('Mobile', 500),
-    new Product('Tv', 1220),
+    new Product('Radio', 122,true),
+    new Product('Walkman', 150, false),
+    new Product('Watch', 200, true),
+    new Product('Mobile', 500, false),
+    new Product('Tv', 1220,true),
 ];
 
-$productNames = array_map(function($product){
-    return $product->name;
-}, $productData);
+$outOfStock = array_filter($productData, function($product) {
+    return !$product->outOfStock; 
+});
 
-dd($productNames);
+
+dd($outOfStock);
 
